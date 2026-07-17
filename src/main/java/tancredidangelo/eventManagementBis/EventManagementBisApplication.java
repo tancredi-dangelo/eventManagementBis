e4ec564172850f8e465a2b8536baabf65f1b3c30
Tancredi <tancredi.dangelo.22@gmail.com>
2026-07-17 10:13:33 +0200

    first commit

---
+++ src/main/java/tancredidangelo/eventManagementBis/EventManagementBisApplication.java
@@ -0,0 +1,13 @@
+package tancredidangelo.eventManagementBis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementBisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementBisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tancredidangelo/eventManagementBis/EventManagementBisApplicationTests.java
@@ -0,0 +1,13 @@
+package tancredidangelo.eventManagementBis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementBisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
